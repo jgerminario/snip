8f3f932a853df9889c2ac3d443aaf9a0d01dc13b
cmliotta <cmliotta@msn.com>
2014-12-03 20:38:01 -0800

    added title date line

--- source/controller.rb
+++ source/controller.rb
@@ -10,6 +10,7 @@ module CommandLineController
     file_read = SourceFileReaderWriter.new(ARGV.first)
     to_run = file_read.convert_to_array_of_lines
     CodeScanner.run(to_run)
+    Snippet.snippet_array
     DestinationFileWriter.run(Snippet.snippet_array)
     puts ViewFormatter.success_message(DestinationFileWriter.full_file_directory)
     file_read.overwrite_existing_snips
--- source/rspec/rspec.rb
+++ source/rspec/rspec.rb
@@ -3,16 +3,16 @@ require_relative '../controller'
 
 describe "This program" do
 
-	# Setting up a test file and test code to work off of 
+	# Setting up a test file and test code to work off of
 	before :all do
 
 		@rspec_test_file = "rspec_test.rb"
 		@num_of_snips = rand(1..10)
-		@test_file_code = 
+		@test_file_code =
 				[
 				 "#<snip>\n",
 				 "5.times {|x| p x}\n",
-				 "#</snip>\n", 
+				 "#</snip>\n",
 				 "\n"
 				 ]*@num_of_snips
 
@@ -86,16 +86,17 @@ describe "This program" do
 		 	end
 
 		 	it "finds tags anywhere in the line, not just adjacent to the # comment symbol" do
-		 		anywhere_code_block = 
-		 				["# filler <snip> filler\n",
+		 		anywhere_code_block =
+		 				["#    erwer <snip>r er \n",
 		 				 "5.times {|x| pppppp x}\n",
-		 				 "#    </snip>  \n",
+		 				 "# rwerewr </snip>  rewre \n",
 		 				 "\n"
 		 				]
 		 		anywhere_num = rand(5)
-		 		test_block = anywhere_code_block*anywhere_num
+		 		p test_block = (anywhere_code_block*anywhere_num)
 		 		CodeScanner.run(test_block)
 		 		p CodeScanner.scan_array
+		 		p Snippet.snippet_array
 		 		expect(Snippet.snippet_array.length).to eq(anywhere_num)
 		 	end
 

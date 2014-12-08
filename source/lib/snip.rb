46018d1b10a61136e162805c956bd885f6cb2c98
jgerminario <jgerminario@gmail.com>
2014-12-07 21:32:46 -0800

    Tweaks to gem implementation

--- source/lib/snip.rb
+++ source/lib/snip.rb
@@ -1,11 +1,17 @@
 require "snip/version"
 require_relative '../config/environment'
 require_relative '../app/controllers/controller'
+require_relative '../app/models/utils/batchprocessing'
 
 module Snip
-	class Test
-		def say_hello
-			CommandLineController.run
+	class Run
+		def execute
+			if ARGV[0].include?(".rb")
+			  CommandLineController.run(ARGV[0])
+			else
+			  BatchProcessing.process(ARGV[0])
+			end
+
 			puts ViewFormatter.success_message(DestinationFileWriter.full_file_directory)
 		end
 	end
--- source/rspec/test_snip_files/subfolder/test3.rb
+++ source/rspec/test_snip_files/subfolder/test3.rb
@@ -1,31 +1,31 @@
-# <*snip*> Title of first snip
+# <snip> Title of first snip
   desc "create the database"
   task :create do
     puts "Creating file #{DB_PATH} if it doesn't exist..."
     touch DB_PATH
   end
-# </*snip*>
+# </snip>
 
-# <*snip*> Title of this snip
+# <snip> Title of this snip
   desc "drop the database"
   task :drop do
     puts "Deleting #{DB_PATH}..."
     rm_f DB_PATH
   end
-# </*snip*>
+# </snip>
 
-# <*snip*> Title of first snip
+# <snip> Title of first snip
   desc "create the database"
   task :create do
     puts "Creating file #{DB_PATH} if it doesn't exist..."
     touch DB_PATH
   end
-# </*snip*>
+# </snip>
 
-# <*snip*> Title of this snip
+# <snip> Title of this snip
   desc "drop the database"
   task :drop do
     puts "Deleting #{DB_PATH}..."
     rm_f DB_PATH
   end
-# </*snip*>
+# </snip>

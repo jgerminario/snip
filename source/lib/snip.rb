595ee3dcb55b5fd08ed9a38c09e9ad0bb6de15ae
jgerminario <jgerminario@gmail.com>
2014-12-07 21:22:59 -0800

    gem tests

--- source/lib/snip.rb
+++ source/lib/snip.rb
@@ -1,9 +1,12 @@
 require "snip/version"
+require_relative '../config/environment'
+require_relative '../app/controllers/controller'
 
 module Snip
 	class Test
 		def say_hello
-			puts "Hello there"
+			CommandLineController.run
+			puts ViewFormatter.success_message(DestinationFileWriter.full_file_directory)
 		end
 	end
   # Your code goes here...

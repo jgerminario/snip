711c49c65e02f6753638c239a236aa00499a6d3f
jgerminario <jgerminario@gmail.com>
2015-02-22 22:56:21 -0800

    refactors batch processing to accommodate multiple language support

--- source/lib/models/utils/batchprocessing.rb
+++ source/lib/models/utils/batchprocessing.rb
@@ -1,8 +1,10 @@
 class BatchProcessing
 
+  require_relative '../../languages/languages'
+
   def self.process(directory)
   	directory = directory.chomp('/')
-    Dir.glob(directory + "/**/*.{rb,js,erb}") do |file|
+    Dir.glob(directory + "/**/*.{#{Language.languages.keys.join(',')}}") do |file|
       next if file == '.' or file == '..'
       CommandLineController.run(file)
     end

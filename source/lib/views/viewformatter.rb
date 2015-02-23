9fff4fcf40adfd8470de801dfd05129fb0d181be
jgerminario <jgerminario@gmail.com>
2015-02-22 22:06:31 -0800

    refactors viewformatter file to better organize/comment on different view cases and add additional methods to be used by controller file

--- source/lib/views/viewformatter.rb
+++ source/lib/views/viewformatter.rb
@@ -1,4 +1,4 @@
-# View component for file writing operations and possibly terminal output
+# View component for file writing operations
 require 'date'
 require_relative '../languages/languages'
 
@@ -6,13 +6,19 @@ module ViewFormatter
 
 	extend self
 
-  def conjunctionator(list, conj="or")
+### utility
+  def conjunctionator(list = Languages.languages, conj="or")
     if list.class.name == "Hash"
       list = list.keys
     end
     str = "#{list[0..-2].join(", ")} #{conj} #{list[-1]}"
   end
 
+  def line_check(line)
+    line ? ':' + line.to_s : nil
+  end
+
+### file output formatting
 	def snippet_indexer(index, title, type)
     languages = Languages.languages
     if languages.key?(type)
@@ -22,43 +28,16 @@ module ViewFormatter
 		end
 	end
 
-	def file_not_found
-		"File not found, please try again."
-	end
-
-	def invalid_file
-		"File must be a #{conjunctionator(Languages.languages)} file."
-	end
-
-	def success_message(filedir)
-		if Snippet.snippet_counter > 0
-			"Your snippet file has been successfully updated with #{Snippet.snippet_counter} new snips at: '#{filedir}'"
-		else
-			"No snips found in the specified files or directories."
-		end
-	end
-
   def status_line(line, type, file)
     languages = Languages.languages
-  	if languages.key?(type)
-   	  "#{languages[type][0]} Snipped from #{file}#{line_check(line)} on #{Time.now.strftime("%m-%d-%Y")} #{languages[type][1]}"
-   	else
-   		"# Snipped from #{file}#{line_check(line)} on #{Time.now.strftime("%m-%d-%Y")}"
-   	end
+    if languages.key?(type)
+      "#{languages[type][0]} Snipped from #{file}#{line_check(line)} on #{Time.now.strftime("%m-%d-%Y")} #{languages[type][1]}"
+    else
+      "# Snipped from #{file}#{line_check(line)} on #{Time.now.strftime("%m-%d-%Y")}"
+    end
   end
 
-  def line_check(line)
-  	line ? ':' + line.to_s : nil
-  end
-
-  def snip_terminal_status(filename, line)
-  	if line
-  		"#{filename}:#{line} snipped"
-  	else
-  		nil
-  	end
-  end
-
+### file management (based on check)config_file function in DestinationFileWriter)
   def specify_path
   	"Please specify where you would like to save your snippets by running 'snip -f <filepath>'"
   end
@@ -71,8 +50,128 @@ module ViewFormatter
   	"New snippet file created at #{File.absolute_path(filename)}"
   end
 
+
+### default snip functionality reporting messages (based on files/directories)
+  def input_file_not_found
+    "File not found, please try again."
+  end
+
+  def invalid_input_file
+    "File must be a #{conjunctionator} file."
+  end
+
+  def success_message(filedir)
+    if Snippet.snippet_counter > 0
+      "Your snippet file has been successfully updated with #{Snippet.snippet_counter} new snips at: '#{filedir}'"
+    else
+      "No snips found in the specified files or directories."
+    end
+  end
+
+  def snip_terminal_status(filename, line)
+    if line
+      "#{filename}:#{line} snipped"
+    else
+      nil
+    end
+  end
+
+### ARGV command line argument-specific
+
+  # no args
+  def no_args_message
+    "Welcome to snip. Type 'snip --help' for help"
+  end
+
+  # filename set (-f)
+	def new_file_path
+		"Snippet file location saved as '#{File.absolute_path(ARGV[1])}/my_snips.txt'"
+	end
+
+	def specify_filename(filename)
+		if filename
+			"Your file is located at #{filename}"
+		else
+			"No output file found, please specify a filename after '-f'"
+		end
+	end
+
+	def need_to_specify_directory
+		"Please specify an existing directory to save your snippets to"
+	end
+
+  # Search (-d)
+	def show_snips(file)
+		if file
+			if File.readlines(file)[0]
+				File.readlines(file)
+			else
+				"Snippet file is empty"
+			end
+		else
+			"Snippet file doesn't exist. Specify where you would like to save your snippets by running 'snip -f <filepath>"	
+		end
+	end
+
+	def display_error
+		"Specify code type (#{conjunctionator}) and/or a search string, e.g. 'snip -d js \"event delegation\"', or snip -d .' for all snips"
+	end
+
+	def no_results
+		"No matching snippet files found"
+	end
+
+  # reindex (-i)
+  def reindexed
+    "Your snippet files have been reindexed"
+  end
+
+  # log (-l)
+  def show_log(log_file)
+    if File.readlines(log_file)[0]
+      File.readlines(log_file)
+    else
+      "Log file is empty"
+    end
+  end
+
+  # whitespace (-w)
+  def whitespace_success
+    "Whitespace restored"
+  end
+
+  # delete (--delete)
+  def delete_error
+    "Please specify which snippets you wish to delete"
+  end
+
+  def delete_success
+    "Snips #{ARGV[1]} deleted from my_snips.txt (other files must be modified manually)"
+  end
+
+  # clipboard (-c)
+  def clipboard_instructions
+    "Run 'snip -c' or specify code type (#{Languages.languages.keys.join(", ") + " or misc"}) and a title string with: 
+ 'snip -c rb \"Using string interpolation\"' "
+  end
+
+  def clipboard_prompts
+    ["Type of code (e.g. #{conjunctionator}) or press enter for misc:", "Title:"]
+  end
+
+  def clipboard_origins(type)
+    type != "misc" ? type_str = " (.#{type})" : type_str = ""
+    "clipboard#{type_str}"
+  end
+
+
+  def clipboard_success(code)
+    "Your code has been snipped:\n\n#{code}"
+  end
+
+  # help (--help)
   def terminal_help_message
-  	"
+    "
 
 *** First time setup: ***
 
@@ -106,7 +205,7 @@ You can alternatively use <$> and </$>. Your tags will be replaced with <*snip*>
   snip <directory> - process a directory recursively
   snip -c - creates new snippet from clipboard contents
   snip -d . - display all your snips in terminal
-  snip -d <type> '<string>' - searches your snips with type #{conjunctionator(Languages.languages)} and/or search string
+  snip -d <type> '<string>' - searches your snips with type #{conjunctionator} and/or search string
   snip -i - reindexes your snippet files (after deleting old snips, etc)
   snip -l - view log history if you need to debug previous snips
   snip --delete <num> - deletes the specified snippet number(s) (comma-delimited) and reindexes
@@ -115,61 +214,5 @@ You can alternatively use <$> and </$>. Your tags will be replaced with <*snip*>
 Visit https://github.com/jgerminario/snip for more information or to submit bug reports/feature requests.
 
 "
-	end
-
-	def new_file_path
-		"Snippet file location saved as '#{File.absolute_path(ARGV[1])}/my_snips.txt'"
-	end
-
-	def no_args_message
-		"Welcome to snip. Type 'snip --help' for help"
-	end
-
-	def specify_filename(filename)
-		if filename
-			"Your file is located at #{filename}"
-		else
-			"No output file found, please specify a filename after '-f'"
-		end
-	end
-
-	def need_to_specify_directory
-		"Please specify an existing directory to save your snippets to"
-	end
-
-	def show_log(log_file)
-		if File.readlines(log_file)[0]
-			File.readlines(log_file)
-		else
-			"Log file is empty"
-		end
-	end
-
-	def show_snips(file)
-		if file
-			if File.readlines(file)[0]
-				File.readlines(file)
-			else
-				"Snippet file is empty"
-			end
-		else
-			"Snippet file doesn't exist. Specify where you would like to save your snippets by running 'snip -f <filepath>"	
-		end
-	end
-
-	def reindexed
-		"Your snippet files have been reindexed"
-	end
-
-	def clipboard_command
-		"Run 'snip -c' or specify code type (#{Languages.languages.keys.join(", ") + " or misc"}) and a title string with: 
- 'snip -c rb \"Using string interpolation\"' "
-	end
-
-	def display_error
-		"Specify code type (#{conjunctionator(Languages.languages)}) and/or a search string, e.g. 'snip -d js \"event delegation\"', or snip -d .' for all snips"
-	end
-	def no_results
-		"No matching snippet files found"
-	end
+  end
 end
